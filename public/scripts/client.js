1577e5a6537b44a24a49436f712b08f840dad4b5
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-27 21:12:58 +0000

    Render tweets dynamically from an array of objects leveraging the function that takes and appends object information into DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,8 +5,18 @@
  */
 $(document).ready(function () {
 
-function createTweetElement(tweet) {
-  const markup = 
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    tweets.forEach(tweet => {
+      // calls createTweetElement for each tweet
+      const post = createTweetElement(tweet)
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').prepend(post);
+    });
+  }
+
+const createTweetElement = function(tweet) {
+  const $tweet = 
     `<article class="submitted-tweet">
       <header class="tweet-header">
         <div class="tweet-user-profile">
@@ -26,26 +36,35 @@ function createTweetElement(tweet) {
       </footer>
     </article>`;
     
-    return markup;
+    return $tweet;
 }
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const $tweet = createTweetElement(tweetData);
+renderTweets(data);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 })
