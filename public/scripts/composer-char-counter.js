35485601aaa34fcba21087580e8e561961e8e588
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-27 04:15:54 +0000

    Refactor and remove commented out lines

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
-
   $("#tweet-text").on("input", function (event) {
     let remainingChar = 140 - $(this).val().length;
     let changingCounter = $(this).siblings().children(".counter").html(remainingChar);
-
-    if (remainingChar < 0) {
-      changingCounter.addClass("minus-counter");
-    } else {
-      changingCounter.removeClass("minus-counter");
-    }
-
+    changingCounter.toggleClass("minus-counter", remainingChar < 0);
   });
 });
