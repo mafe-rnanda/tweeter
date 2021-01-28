b2cf8f236dfbad885c682e0489c4ae0a805ba11b
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-28 00:20:06 +0000

    Create an ajax post to the form submission and prevent from redirecting or page reloading

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -64,4 +64,34 @@ $(document).ready(function () {
   };
 
   renderTweets(data);
+
+
+  // AJAX POST request
+
+  //$.post( "test.php", $( "#testform" ).serialize() );
+
+ 
+
+  // Attach a submit handler to the form
+$(".tweet-submission").submit(function(event) {
+  
+  // Stop form from submitting normally
+  event.preventDefault();
+  console.log('button should not redirect me');
+
+  // serialize the form data for submission to the server
+  const serializedData = $(this).serialize();
+  console.log(serializedData);
+
+  $.ajax({
+    type: "POST",
+    url: '/tweets/',
+    data: serializedData,
+    success: function() {
+      console.log('good job')
+    },
+  });
+
+});
+
 });
