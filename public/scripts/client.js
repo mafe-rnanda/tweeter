a60b693e2e14cf4bc930bdb3d0301c280c5d214b
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-29 05:26:30 +0000

    Clean up the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-
   const createTweetElement = function (tweet) {
-    const time = new Date(tweet.created_at)
-    const currentTime = Date.now()
-    const daysAgo = Math.floor((currentTime - time) / 1000 / 60 / 60 / 24)
+    const time = new Date(tweet.created_at);
+    const currentTime = Date.now();
+    const daysAgo = Math.floor((currentTime - time) / 1000 / 60 / 60 / 24);
 
-    const escape =  function(str) {
-      let div = document.createElement('div');
+    // Avoid XSS
+    const escape = function (str) {
+      let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
-    }
-    //tweet.created_at
+    };
 
     const $tweet = `<article class="submitted-tweet">
         <header class="tweet-header">
@@ -35,59 +34,57 @@ $(document).ready(function () {
           </div>
         </footer>
       </article>`;
-  
+
     return $tweet;
   };
 
   const renderTweets = function (tweets) {
-    // remove all children from the <section>
     $("#tweets-container").empty();
-    // loops through tweets
     tweets.forEach((tweet) => {
-      // calls createTweetElement for each tweet
       const post = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
       $("#tweets-container").prepend(post);
     });
   };
 
   // Display tweets as soon as the page loads
-  const loadtweets = function() {
+  const loadtweets = function () {
     $.ajax({
-      url: '/tweets/',
-      method: 'GET',
-      dataType: 'json',
+      url: "/tweets/",
+      method: "GET",
+      dataType: "json",
       success: (tweet) => {
-        renderTweets(tweet)
+        renderTweets(tweet);
       },
-    })
-  }
+    });
+  };
 
   loadtweets();
 
   // AJAX post > send tweet text to the server and get it displaying on the page
-  $(".tweet-submission").submit(function(event) {
-    const textArea = $('#tweet-text')
+  $(".tweet-submission").submit(function (event) {
+    const textArea = $("#tweet-text");
     if (textArea.val().trim() === "" || textArea.val().length < 0) {
-      $('.error-message').text(` Error: Please enter text`).prepend('<i class="fas fa-exclamation-circle"></i>').slideDown();
+      $(".error-message")
+        .text(` Error: Please enter text`)
+        .prepend('<i class="fas fa-exclamation-circle"></i>')
+        .slideDown();
       return false;
     }
-    
+
     if (textArea.val().length > 140) {
-      $('.error-message').text(`Error: You have exceeded the character limit`).prepend('<i class="fas fa-exclamation-circle"></i>').slideDown();
+      $(".error-message")
+        .text(`Error: You have exceeded the character limit`)
+        .prepend('<i class="fas fa-exclamation-circle"></i>')
+        .slideDown();
       return false;
     }
 
-    $('.error-message').slideUp();
-    $.post('/tweets/', $(this).serialize())
+    $(".error-message").slideUp();
+    $.post("/tweets/", $(this).serialize());
     loadtweets();
-    $("#tweet-text").val('')
+    $("#tweet-text").val("");
     $(".counter").text("140");
-    
-    
+
     event.preventDefault();
-
   });
-
 });
-
